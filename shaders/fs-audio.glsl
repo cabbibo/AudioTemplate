4f9e09292bac071d451f62b7d447f849dc2042fe
cabbibo <isaaclandoncohen@gmail.com>
2015-04-04 13:00:20 -0700

    updating

--- shaders/fs-audio.glsl
+++ shaders/fs-audio.glsl
@@ -1,18 +1,15 @@
 uniform sampler2D t_audio;
 
-varying vec3 vNorm;
 varying vec4 vAudio;
 varying float vAudioLookup;
 
 void main(){
 
   vec4 audio = texture2D( t_audio , vec2( vAudioLookup , 0. ) );
-  // the base color will be the normal
-  vec3 col = vNorm * .5 + .5;
-
+  
   // We are also going to color our fragments
   // based on the color of the audio
-  col *= 1.5 * audio.xyz;
+  vec3 col = audio.xyz;
 
   gl_FragColor = vec4( col , 1. );
 
--- shaders/vs-audio.glsl
+++ shaders/vs-audio.glsl
@@ -1,19 +1,19 @@
-
 uniform sampler2D t_audio;
+uniform float time;
 
 varying vec4 vAudio;
 
 varying float vAudioLookup;
 
-varying vec3 vNorm;
+$simplex
 
+//vec3 getPos( vec3 position 
 void main(){
 
-  //Passing along the normal to the
-  //Fragment shader
-  vNorm = normal;
+  
+  float displacement = snoise( vec4( position * 4. , time * .1 ) );
 
-  vAudioLookup =  abs( normal.x * normal.y * normal.z * 3. );
+  vAudioLookup =  abs( displacement ); //abs( normal.x * normal.y * normal.z * 3. );
   // Here we sample the audio from a texture that we will
   // be creating on the cpu
   vec4 audio = texture2D( t_audio , vec2( vAudioLookup , 0. ));
@@ -23,7 +23,7 @@ void main(){
 
   // To visualize the audio, we will displace the position
   // by a value based on the audio, along the normal
-  vec3 pos = position + .1 * length( audio ) * normal;
+  vec3 pos = position + .1 * displacement * length( audio)  * normal;
 
   // Use this position to get the final position 
   gl_Position = projectionMatrix * modelViewMatrix * vec4( pos , 1.);
